a093445164937a6e8b9b7cf791b21b9bdab2878c
Ricol Wang <ricol@opensimsim.com>
2021-02-09 19:15:20 +0800

    update

--- is_palindrome.playground/Contents.swift
+++ is_palindrome.playground/Contents.swift
@@ -1,12 +1,27 @@
-import Cocoa
-
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        return false
+        if x < 0 { return false }
+        
+        var digits = [Int]()
+        var X = x
+        while X > 0
+        {
+            digits.append(X % 10)
+            X /= 10
+        }
+        
+        var i = 0, j = digits.count - 1
+        while i != j && i < digits.count && j >= 0
+        {
+            if digits[i] != digits[j] { return false }
+            i += 1
+            j -= 1
+        }
+        return true
     }
 }
 
-for (input, answer) in [(121, true), (-121, false), (10, false), (-101, false)]
+for (input, answer) in [(0, true), (1, true), (11, true), (121, true), (-121, false), (10, false), (-101, false)]
 {
     let result = Solution().isPalindrome(input)
     print("input: \(input), result: \(result), answer: \(answer) -> \(result == answer ? "pass" : "fail")")
