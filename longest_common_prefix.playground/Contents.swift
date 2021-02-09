e7eff53bb2a0ce06daf64580fc057a6c898effd9
Ricol Wang <ricol.wang@evopayments.com>
2021-02-09 15:05:15 +0800

    update

--- longest_common_prefix.playground/Contents.swift
+++ longest_common_prefix.playground/Contents.swift
@@ -2,11 +2,36 @@ class Solution
 {
     func longestCommonPrefix(_ strs: [String]) -> String
     {
-        return ""
+        var s = [Character]()
+        var arrays = [[Character]]()
+        var i = 0
+        var min = Int.max
+        strs.forEach { (s) in
+            arrays.append([Character](s))
+            if s.count < min { min = s.count }
+        }
+        while i < min
+        {
+            var bSame = true
+            if let c = arrays.first?[i]
+            {
+                arrays.forEach { (chars) in
+                    if chars[i] != c
+                    {
+                        bSame = false
+                        return
+                    }
+                }
+                if bSame { s.append(c) } else { break }
+                i += 1
+            }else { break }
+        }
+        
+        return String(s)
     }
 }
 
-for (input, answer) in [(["flower","flow","flight"], "fl"), (["dog","racecar","car"], "")]
+for (input, answer) in [(["flower","flow","flight"], "fl"), (["dog","racecar","car"], ""), (["cir", "car"], "c")]
 {
     let result = Solution().longestCommonPrefix(input)
     print("input: \(input), result: \(result), answer: \(answer) -> \(result == answer ? "pass" : "fail")")
