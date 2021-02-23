9a4653ce9ea4aa035b37ee7daa51917c03481ea5
Ricol Wang <ricol.wang@evopayments.com>
2021-02-23 15:53:39 +0800

    update

--- sqrtx.playground/Contents.swift
+++ sqrtx.playground/Contents.swift
@@ -1,29 +1,63 @@
-class Solution
+public class ListNode
 {
-    func mySqrt(_ x: Int) -> Int
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    public func to_array() -> [Int]
     {
-        var max = x, min = 0, r = Int((max + min) / 2), p = 0
-        while max - min > 1
+        var data = [Int]()
+        var temp: ListNode? = self
+        repeat
         {
-            p = r * r
-            if p > x
-            {
-                max = r
-            }else if p < x
-            {
-                min = r
-            }else
-            {
-                return r
-            }
-            r = Int((max + min) / 2)
+            data.append(temp!.val)
+            temp = temp!.next
+        }while temp != nil
+        return data
+    }
+    
+    static func to_list(data: [Int]) -> ListNode?
+    {
+        var h: ListNode?
+        var p: ListNode?
+        for i in data
+        {
+            let n = ListNode()
+            n.val = i
+            if p == nil { h = n; p = n; }
+            else { p?.next = n; p = n }
         }
-        return x == 1 ? 1 : r
+        return h
     }
 }
 
-for (input, answer) in [(4, 2), (8, 2), (9, 3), (10, 3), (1, 1), (0, 0)]
+class Solution
 {
-    let r = Solution().mySqrt(input)
-    print("\(input), \(answer), \(r)...\(r == answer ? "pass" : "fail")")
+    func deleteDuplicates(_ head: ListNode?) -> ListNode?
+    {
+        var h = head, t = h, current: ListNode? = head == nil ? nil : head?.next
+        while let c = current
+        {
+            if c.val != t?.val { t?.next = c; t = c; }
+            current = current?.next
+        }
+        t?.next = nil
+        return h
+    }
+}
+
+for (input, answer) in [([1, 1, 2], [1, 2]), ([1, 1, 2, 3, 3], [1, 2, 3])]
+{
+    if let list1 = ListNode.to_list(data: input)
+    {
+        let data = list1.to_array()
+        if let r = Solution().deleteDuplicates(list1)
+        {
+            let d = r.to_array()
+            let correct = answer == d
+            print("data: \(data), answer: \(answer), mine: \(d) -> \(correct ? "pass" : "fail")")
+        }
+    }
 }
